4c5ac2c31cb51788dde7d1d94dc4b070f1c89df4
Mohamed Hanini <mohamed.hanini95@gmail.com>
2020-07-01 12:04:11 +0100

    editing of example component

---
+++ components/example-component.vue
@@ -0,0 +1,20 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name:"exampel-component",
+mounted () {
+    this.$hello('mounted')
+    // will console.log 'Hello mounted!'
+  },
+  asyncData ({ $hello }) {
+    $hello('asyncData')
+  }
+}
+</script>
+
+<style>
+
+</style>

0223660095c53de994cc271127e75984caf73194
Mohamed Hanini <mohamed.hanini95@gmail.com>
2020-07-01 12:14:40 +0100

    end point

--- components/example-component.vue
+++ components/example-component.vue
@@ -1,5 +1,5 @@
 <template>
-  
+  <h1>{{ this.$hello('test from compenent') }}</h1>
 </template>
 
 <script>
--- plugins/hello.js
+++ plugins/hello.js
@@ -1,5 +1,7 @@
 export default (context, inject) => {
-    const hello = (msg) => console.log(`Hello ${msg}!`)
+    const hello = (msg) => `Hello ${msg}!`
+
+    //const hello = (msg) => console.log(`Hello ${msg}!`)
     // Inject $hello(msg) in Vue, context and store.
     inject('hello', hello)
     // For Nuxt <= 2.12, also add 👇

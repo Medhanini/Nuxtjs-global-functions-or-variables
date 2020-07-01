bc9edcff97c28dbf3c5045a19ec426e49adb16e4
Mohamed Hanini <mohamed.hanini95@gmail.com>
2020-07-01 11:59:10 +0100

    modification of inject function

--- plugins/inject.js
+++ plugins/inject.js
@@ -1,10 +1,7 @@
-const test = {
-    name: 'Test'
-    ping() {
-        console.log('Ping')
-    }
-  }
-  
-  export default ({ app }, inject) => {
-    inject('test', test)
+export default (context, inject) => {
+    const hello = (msg) => console.log(`Hello ${msg}!`)
+    // Inject $hello(msg) in Vue, context and store.
+    inject('hello', hello)
+    // For Nuxt <= 2.12, also add 👇
+    context.$hello = hello
   }
